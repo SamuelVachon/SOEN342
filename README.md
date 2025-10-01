973ab3185e1cca32c59d26e4db3f8edac0da3929
Sergio Abreo Alvarez <pergioa@gmail.com>
2025-10-01 10:31:55 -0400

    added class for train connections and added makefile; updated readme to have new lines for each name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SOEN342
 
-Samuel Vachon 40281580
-Sergio Abreo Alvarez 40274677
-Hossam Mostafa 40245337
+Samuel Vachon 40281580  
+Sergio Abreo Alvarez 40274677  
+Hossam Mostafa 40245337  
--- src/driver.java
+++ src/driver.java
@@ -1,6 +1,12 @@
 
 import java.util.*;
-public class driver {
 
-    
+public class driver {
+    public static void main(String[] args) {
+        TrainConnection.loadTrainConnectionsFromCSV("./eu_rail_network.csv");
+        for (TrainConnection tc : TrainConnection.trainConnections) {
+            System.out.println(tc);
+        }
+        System.out.println(TrainConnection.trainConnections.size());
+    }
 }
